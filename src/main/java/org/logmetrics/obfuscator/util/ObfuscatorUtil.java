5c1946ee59587c8d271c6b5ef246e989fb810197
Karthikeyan Sadayamuthu <ksaday200@cable.comcast.com>
2022-02-26 02:42:06 -0500

    initial commit

---
+++ src/main/java/org/logmetrics/obfuscator/util/ObfuscatorUtil.java
@@ -0,0 +1,143 @@
+package org.logmetrics.obfuscator.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by org.logmetrics on 5/2/17.
+ */
+public class ObfuscatorUtil {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ObfuscatorUtil.class);
+
+    public static final String MASK_FIXED = "********";
+
+
+
+    public static final String STRATEGY_MD5 = "md5";
+    public static final String STRATEGY_SHA256 = "sha256";
+    public static final String STRATEGY_MASK_FIXED = "fixedMask";
+    public static final String STRATEGY_MASK_MATCHED = "matchedMask";
+    public static final String STRATEGY_NONE = "none";
+
+    public static final List<String> STRATEGIES = Collections.unmodifiableList(new ArrayList<String>() {{
+        add(STRATEGY_MD5);
+        add(STRATEGY_SHA256);
+        add(STRATEGY_MASK_FIXED);
+        add(STRATEGY_MASK_MATCHED);
+        add(STRATEGY_NONE);
+    }});
+
+    public static String obfuscate(String strategy, String value) {
+        if(StringUtils.isBlank(strategy)) {
+            return value;
+        } else if(STRATEGIES.contains(strategy)) {
+            switch(strategy) {
+                case STRATEGY_MD5:
+                    return applyHashMd5(value);
+                case STRATEGY_SHA256:
+                    return applyHashSha256(value);
+                case STRATEGY_MASK_MATCHED:
+                    return  applyMatchedMask(value);
+                case  STRATEGY_NONE:
+                    return value;
+                default:
+                    return applyFixedMask(value);
+            }
+        } else {
+            LOG.warn("Unknown obfuscation strategy: {}",strategy);
+            return applyFixedMask(value);
+        }
+    }
+
+    public static Map<String,String> createFieldStrategyMap(Map<String, Set<String>> strategyFieldMap) {
+        Map<String,String> obfuscateMap = new HashMap<>();
+        strategyFieldMap.forEach((key,values) -> {
+            values.forEach(value -> {
+                if(obfuscateMap.containsKey(value)) {
+                    LOG.warn("Obfuscation strategy already declared for field '{}'.",value);
+                }
+                obfuscateMap.put(value,key);
+            });
+        });
+        return obfuscateMap;
+    }
+
+    public static String applyFixedMask(String target) {
+        return MASK_FIXED;
+    }
+
+    public static String applyMatchedMask(String target) {
+        return StringUtils.isBlank(target) ? target : StringUtils.leftPad("",target.length(),"*");
+    }
+
+    public static String applyHashMd5(String target) {
+        if(StringUtils.isBlank(target)) {
+            return target;
+        } else {
+            try {
+                return DigestUtils.md5Hex(target.getBytes(StandardCharsets.UTF_8));
+            } catch(RuntimeException rtEx) {
+                LOG.error("Unknown error while trying to hash text. Returning default fixed mask string.", rtEx);
+                return applyFixedMask(target);
+            }
+        }
+    }
+
+    public static String applyHashSha256(String target) {
+        if(StringUtils.isBlank(target)) {
+            return target;
+        } else {
+            try {
+                return DigestUtils.sha256Hex(target.getBytes(StandardCharsets.UTF_8));
+            } catch(RuntimeException rtEx) {
+                LOG.error("Unknown error while trying to hash text. Returning default fixed mask string.", rtEx);
+                return applyFixedMask(target);
+            }
+        }
+    }
+    public static void processJsonMap(Map<String, Object> jsonMap, Map<String,String> fieldStrategyMap) {
+        jsonMap.forEach((fieldName,value) -> {
+            if(fieldStrategyMap.containsKey(fieldName)) {
+                jsonMap.put(fieldName,processJsonField(fieldName,value,fieldStrategyMap));
+            } else if(value instanceof Map) {
+                processJsonMap((Map<String,Object>)value,fieldStrategyMap);
+            } else if(value instanceof List) {
+                ((List)value).forEach(listVal -> {
+                    if(listVal instanceof Map) {
+                        processJsonMap((Map<String,Object>)listVal,fieldStrategyMap);
+                    }
+                });
+            }
+        });
+    }
+
+    public static Object processJsonField(String fieldName, Object value, Map<String,String> fieldStrategyMap) {
+        if(value==null) {
+            return value;
+        } else if(value instanceof String) {
+            return obfuscate(fieldStrategyMap.get(fieldName), (String) value);
+        } else if(value instanceof Number) {
+            return obfuscate(fieldStrategyMap.get(fieldName), String.valueOf(value));
+        } else if(value instanceof String[]) {
+            return Arrays.stream((String[]) value).map(arrayVal -> obfuscate(fieldStrategyMap.get(fieldName), arrayVal)).toArray(String[]::new);
+        } else if(value instanceof List) {
+            return ((List<Object>) value)
+                    .stream()
+                    .map(listVal -> processJsonField(fieldName, listVal, fieldStrategyMap))
+                    .collect(Collectors.toList());
+        } else {
+            LOG.warn("Unknown JSON structure encountered.  Fix your code: {}",value.getClass().getName());
+            return value;
+        }
+    }
+    public static String extractFieldRegex(Map<String,Set<String>> fieldMap) {
+        return fieldMap.entrySet().stream().map(Map.Entry::getValue).distinct().map(value -> String.join("|",value)).collect(Collectors.joining("|"));
+    }
+}
---
+++ src/test/java/org/logmetrics/obfuscator/ObfuscatorTest.java
@@ -0,0 +1,249 @@
+package org.logmetrics.obfuscator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ObfuscatorTest {
+
+    @Test
+    public void testSimpleJson() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("obfuscate", items);
+        String response = Obfuscator.obfuscateJsonString(simpleJson,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleJson_none() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("none", items);
+        String response = Obfuscator.obfuscateJsonString(simpleJson,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleJson_md5() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("md5", items);
+        String response = Obfuscator.obfuscateJsonString(simpleJson,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleJson_sha256() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("sha256", items);
+        String response = Obfuscator.obfuscateJsonString(simpleJson,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleJson_matchedMask() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("matchedMask", items);
+        String response = Obfuscator.obfuscateJsonString(simpleJson,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testJsonList() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("Obfuscate", items);
+        String response = Obfuscator.obfuscateJsonString(jsonList,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testJsonList_md5() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("md5", items);
+        String response = Obfuscator.obfuscateJsonString(jsonList,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testJsonList_none() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("none", items);
+        String response = Obfuscator.obfuscateJsonString(jsonList,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testJsonList_sha256() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("sha256", items);
+        String response = Obfuscator.obfuscateJsonString(jsonList,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testJsonList_matchedMask() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("matchedMask", items);
+        String response = Obfuscator.obfuscateJsonString(jsonList,obfuscatorMap);
+        System.out.println(response);
+    }
+
+    @Test
+    public void testSimpleXML() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("Obfuscate", items);
+        String response = Obfuscator.obfuscateXmlString(simpleXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleXML_none() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("none", items);
+        String response = Obfuscator.obfuscateXmlString(simpleXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleXML_md5() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("md5", items);
+        String response = Obfuscator.obfuscateXmlString(simpleXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testSimpleXML_sha256() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("sha256", items);
+        String response = Obfuscator.obfuscateXmlString(simpleXML,obfuscatorMap);
+        System.out.println(response);
+    }
+
+    @Test
+    public void testComplexXML() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("Obfuscate", items);
+        String response = Obfuscator.obfuscateXmlString(complexXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testComplexXML_none() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("none", items);
+        String response = Obfuscator.obfuscateXmlString(complexXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testComplexXML_md5() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("md5", items);
+        String response = Obfuscator.obfuscateXmlString(complexXML,obfuscatorMap);
+        System.out.println(response);
+    }
+    @Test
+    public void testComplexXML_sha256() {
+        Map<String, Set<String>> obfuscatorMap= new HashMap<>();
+        Set<String> items = new HashSet<>();
+        items.add("name");
+        items.add("salary");
+        items.add("age");
+        obfuscatorMap.put("sha256", items);
+        String response = Obfuscator.obfuscateXmlString(complexXML,obfuscatorMap);
+        System.out.println(response);
+    }
+
+    private String simpleJson = "{\"id\":1,\"name\":\"karthik\",\"age\":29,\"salary\":115000}";
+    private String jsonList = "{\"user\":[{\"id\":1,\"name\":\"karthik\",\"age\":29,\"salary\":115000},{\"id\":2,\"name\":\"Karthikeyan Sadayamuthu\",\"age\":30,\"salary\":500000},{\"id\":3,\"name\":\"Sadayamuthu\",\"age\":30,\"salary\":500000},{\"id\":4,\"name\":\"Vinodh Kannan Sadayamuthu\",\"age\":30,\"salary\":500000}]}";
+    private String simpleXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+            "<root>\n" +
+            "  <id>1</id>\n" +
+            "  <name>karthik</name>\n" +
+            "  <age>29</age>\n" +
+            "  <salary>115000</salary>\n" +
+            "</root>";
+    private String complexXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+            "<root>\n" +
+            "  <user>\n" +
+            "    <id>1</id>\n" +
+            "    <name>karthik</name>\n" +
+            "    <age>29</age>\n" +
+            "    <salary>115000</salary>\n" +
+            "  </user>\n" +
+            "  <user>\n" +
+            "    <id>2</id>\n" +
+            "    <name>Karthikeyan Sadayamuthu</name>\n" +
+            "    <age>30</age>\n" +
+            "    <salary>500000</salary>\n" +
+            "  </user>\n" +
+            "  <user>\n" +
+            "    <id>3</id>\n" +
+            "    <name>Sadayamuthu</name>\n" +
+            "    <age>30</age>\n" +
+            "    <salary>500000</salary>\n" +
+            "  </user>\n" +
+            "  <user>\n" +
+            "    <id>4</id>\n" +
+            "    <name>Vinodh Kannan Sadayamuthu</name>\n" +
+            "    <age>30</age>\n" +
+            "    <salary>500000</salary>\n" +
+            "  </user>\n" +
+            "</root>";
+
+}
